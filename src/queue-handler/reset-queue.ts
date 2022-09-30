d6f42a0e15c8355ba6b9ce683004a36200f15e19
Petter Kaspersen <petter@kaspersen.dev>
2022-09-30 05:23:45 +0200

    Remove unused handler

--- src/queue-handler/reset-queue.ts
+++ src/queue-handler/reset-queue.ts
@@ -1,9 +1,13 @@
 import db from "../db/db";
 
-export async function resetQueue(channelId: string) {
-  await db.queuePlayer.deleteMany({
-    where: {
-      channel_id: channelId,
-    },
-  });
+export async function resetQueue(channelId?: string) {
+  if (channelId) {
+    await db.queuePlayer.deleteMany({
+      where: {
+        channel_id: channelId,
+      },
+    });
+  } else {
+    await db.queuePlayer.deleteMany();
+  }
 }
